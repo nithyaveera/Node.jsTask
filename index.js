1143ba742deb12df4ed82135a13871a2ac1d2a13
nithyaveera <nithyaveeramani2003@gmail.com>
2024-03-05 19:59:36 +0530

    update-task

--- index.js
+++ index.js
@@ -9,13 +9,54 @@ app.get('/', (req, res) => {
     let current_date = format(new Date(), 'dd-MM-yyyy-HH-mm-ss', { timeZone: 'Asia/Kolkata' })
     const filepath = `Timestamp/current-date-time.txt`
     fs.writeFileSync(filepath, `${current_date}`, 'utf-8');
-    res.status(200).send(`${current_date}`)
+    res.status(200).send(`<html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        text-align: center;
+                        padding: 20px;
+                    }
+                    .timestamp {
+                        font-size: 24px;
+                        color: #333;
+                        margin-bottom: 20px;
+                    }
+                    #end{
+                        color:#5E5241
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="timestamp">Current Date and Time: ${current_date} </div>
+                <div class="timestamp">To Retrieve All TimeStamp Data Change API Endpoint to <span id='end'>/read </span></div>
+
+            </body>
+        </html>`)
 })
 
 app.get('/read', (req, res) => {
     const filepath = `Timestamp/current-date-time.txt`
     const readData = fs.readFileSync(filepath, 'utf-8');
-    res.status(200).send(`${readData}`)
+    res.status(200).send(`<html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        text-align: center;
+                        padding: 20px;
+                    }
+                    .timestamp {
+                        font-size: 24px;
+                        color: #333;
+                        margin-bottom: 20px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="timestamp">Read Data: ${readData}</div>
+            </body>
+        </html>`)
 })
 
 app.listen(PORT, () => {
